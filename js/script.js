5e97ae20ec4384a06646557269105ed4013eff0f
Vladimír_nickname <112708010+vladimirnickname@users.noreply.github.com>
2023-02-27 17:27:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,6 +35,23 @@ $(document).ready(function () {
 });
 
 
+validateForms("#contacts__form");
+
+$("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize(),
+    }).done(function () {
+        $(this).find("input").val("");
+        $("form").trigger("reset");
+    });
+    return false;
+});
+
+
+
 
 
 
